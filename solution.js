173a3c43c2eaf5f7729ca274e12f1501ee4ce660
Cem <cemkgrbz@gmail.com>
2022-07-11 14:51:53 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,61 @@
+//1
+
+const isDog = true;
+console.log(isDog == true ? 'pat, pat': 'find me a dog to pat!');
+
+//2
+
+const speedLimit = 50, yourSpeed = 75;
+if (speedLimit > yourSpeed) {
+    console.log('You are driving below the speed limit, Oma')
+} else {
+    console.log('You are going too fast!')
+};
+
+//3
+
+const age = 27;
+if (age < 16) {
+    console.log('Serve butter beer')
+} else {
+    console.log('Serve beer')
+};
+
+//4
+
+const isStudent = true;
+if (isStudent === true) {
+    console.log('Ticket costs €5,00')
+} else {
+    console.log('Ticket costs €12,00')
+};
+
+//5
+
+const okMarie = 'cake';
+if (okMarie === 'cake') {
+    console.log('Let them eat cake')
+} else {
+    console.log('Oh, bother')
+};
+
+//6
+
+const num1 = 30, num2 = 939, num3 = 40.9;
+if (30 % 2 === 0) {
+    console.log('30 is even')
+} else {
+    console.log('31 is odd')
+}
+
+if (939 % 2 === 0) {
+    console.log('939 is even')
+} else {
+    console.log('939 is odd')
+}
+
+if (40.9 % 2 === 0) {
+    console.log('40.9 is even')
+} else {
+    console.log('40.9 is odd')
+}
